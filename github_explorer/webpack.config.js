5542dede3030331cebe9939a2f9f37f13d02f9a6
Rafael Reis <rafaelreis@gurgelmix.com.br>
2021-11-01 23:22:18 -0300

    configuração do webpack

--- github_explorer/webpack.config.js
+++ github_explorer/webpack.config.js
@@ -1,21 +1,39 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
-    output: {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map', //source-map do javascript para ver o js real
+    entry: path.resolve(__dirname, 'src', 'index.jsx'), //arquivo de entrada
+    output: { // arquivo de saida
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+    devServer: { // servidor do webpack para reload da aplicação
+        static: path.resolve(__dirname, 'public', 'index.html')
+    },
+    plugins: [ // plugin para add o js no html automaticamente
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
     module: {
         rules: [
-            {
+            { // loader para converter react em js que o browser entende
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
-            }
+            },
+            { // loader para converter sass em css
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
         ],
     }
 };
