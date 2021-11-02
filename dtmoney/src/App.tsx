960dec2d1c682cab9446b832546bf4fe3f424bab
Rafael Reis <rafaelreis@gurgelmix.com.br>
2021-11-02 16:31:04 -0300

    adição de codigo para:
    - seed no miragejs
    - componente de modal
    - inserir item no miragejs
    - props no css

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
+
 export function App() {
+  const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <NewTransactionModal isOpen={isNewTransactionOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
     </>
   );
