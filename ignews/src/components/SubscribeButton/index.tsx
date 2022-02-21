9aadb3a9e2d81e8c8435806c9108be8afedbeb88
Rafael Reis <rafaelreis@gurgelmix.com.br>
2022-02-21 20:04:21 -0300

    alterações para o ignews

---
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+interface subscribeButtonProps {
+    priceId: string;
+}
+
+export function SubscribeButton({ priceId }: subscribeButtonProps) {
+    return (
+        <button type="button" className={styles.subscribeButton}>Subscribe now</button>
+    );
+}
--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,14 +1,55 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import { SubscribeButton } from '../components/SubscribeButton';
 
-export default function Home() {
+import styles from './home.module.scss';
+import { stripe } from '../services/stripe';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+};
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head> 
-        <title>Inicio | ig.news</title> 
+        <title>Home | ig.news</title> 
       </Head>
-      <h1> 
-        hello world
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <img src="/assets/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Jz9aQIlDZFFNsyjndMTQyuU');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
+  };
+};
