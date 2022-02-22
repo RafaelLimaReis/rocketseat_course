66e19bbc5592439c6ffd9d1022b6543c515ed157
Rafael Reis <rafaelreis@gurgelmix.com.br>
2022-02-22 20:45:50 -0300

    adição de login com github e salvamento de info no faunadb

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github";
+import { query as q } from 'faunadb';
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -8,4 +11,38 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const { email: emailUser } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(emailUser)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email: emailUser }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(emailUser)
+              )
+            )
+          )
+        );
+  
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 })
