c13326abb0a2e010e113d87f5894336d9d50be56
Rafael Reis <rafaelreis@gurgelmix.com.br>
2022-02-28 10:56:10 -0300

    uso do activelink e validação de inscrição do usuario

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
@@ -10,11 +11,17 @@ interface subscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: subscribeButtonProps) {
+    const router = useRouter();
     const { data: session } = useSession();
 
     async function handleSubscribe() {
         if (!session) {
             signIn('github');
+            return;
+        }
+
+        if (session.activeSubscription) {
+            router.push('/posts');
             return;
         }
 
