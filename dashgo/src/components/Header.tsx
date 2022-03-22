eb98ebdc23913904a38698d50093e0419ce31062
Rafael Reis <rafaelreis@gurgelmix.com.br>
2022-03-21 22:33:08 -0300

    finalizando estilo do header

--- dashgo/src/components/Header.tsx
+++ dashgo/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserLine } from 'react-icons/ri';
 
 export function Header() {
     return (
@@ -24,6 +24,23 @@ export function Header() {
             >
                 <Input color="gray.500" variant="unstyled" placeholder="Buscar na plataforma" px="4" mr="4" _placeholder={{ color: 'gray.400'}}/>
                 <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                <HStack spacing="8" mx="8" pr="8" py="1" color="gray.300" borderRightWidth={1} borderColor="gray.700">
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserLine} fontSize="20" />
+                </HStack>
+                <Flex align="center">
+                    <Box mr="4" textAlign="right">
+                        <Text>Rafael Lima</Text>
+                        <Text color="gray.300" fontSize="small">rafaellimareis01@gmail.com</Text>
+                    </Box>
+                    <Avatar size="md" name="Rafael Lima" src="https://github.com/RafaelLimaReis.png"/>
+                </Flex>
             </Flex>
         </Flex>
     );
